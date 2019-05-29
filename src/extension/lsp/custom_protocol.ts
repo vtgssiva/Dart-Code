c7d3b749c47d06ca3f61afb278d50c9afe7ae8b6
Danny Tuppeny <danny@tuppeny.com>
2019-05-29 14:41:11 +0100

    Wire up analysis status events for LSP

---
+++ src/extension/lsp/custom_protocol.ts
@@ -0,0 +1,9 @@
+import { NotificationType } from "vscode-languageclient";
+
+export class AnalyzerStatusNotification {
+	public static type = new NotificationType<AnalyzerStatusParams, void>("$/analyzerStatus");
+}
+
+export interface AnalyzerStatusParams {
+	readonly isAnalyzing: boolean;
+}
